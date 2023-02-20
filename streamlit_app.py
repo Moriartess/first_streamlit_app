1e2d9466b4c61db7f5c161f29bc7c65bb7c13301
Moriartess <45066222+Moriartess@users.noreply.github.com>
2023-02-20 04:13:50 +0100

    Update streamlit_app.py
    added try block to insert a fruit part

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,5 +66,9 @@ def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
        my_cur.execute("insert into fruit_load_list values ('" + new_fruit +"')")
        return "Thanks for adding " + new_fruit
-add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-insert_row_snowflake(add_my_fruit)
+try:
+  add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+  if not add_my_fruit:
+    streamlit.text('Enter the fruit you\'d like to add')
+  else:
+    insert_row_snowflake(add_my_fruit)
