bdfdef524a22ccc76494f9710f4b81aa730d802e
Moriartess <45066222+Moriartess@users.noreply.github.com>
2023-02-20 04:28:31 +0100

    Update streamlit_app.py
    turned text above the button into header

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ except URLError as e:
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 
-streamlit.text("View Our Fruit List - Add Yout Favourites")
+streamlit.header("View Our Fruit List - Add Yout Favourites!")
 # Snowflake-related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
